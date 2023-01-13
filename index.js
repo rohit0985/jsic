2608dbbc4d280d1659c9343882acfdac54482606
rohit0985 <rohitsharma4681@gmail.com>
2023-01-13 17:57:46 +0530

    added new code for index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
-server.use("/", router);
+server.use(router);
 server.listen(PORT, () =>
   console.log(`JSON Server is running on port ${PORT}`)
 );
